4dc09cf7494da6b53981c4d4e64a184efeaa61d4
E. Soubies <emmanuel.soubies@irit.fr>
2020-10-29 09:53:30 +0100

    reorganization

--- ScriptPatternFromFairSIM.m
+++ ScriptPatternFromFairSIM.m
@@ -5,12 +5,18 @@
 % Copyright (2020) E. Soubies emmanuel.soubies@irit.fr
 %--------------------------------------------------------------------------
 clear; clc;
-addpath ../Utils/
+addpath Utils/
 
-% The file FairSimParameters.xml contains the patterns parameters for the 
+% The file Data/FairSimParameters.xml contains the patterns parameters for the 
 % SIM images in the Data/ folder that have been estimated with the FairSIM 
 % ImageJ plugin. 
 
 % Below is the line to execute in order to obtain the SIM patterns saved as
 % patterns.tif in the Data/ folder.
-[patt,~,~,~]=PatternsFromFairSimXml('FairSimParameters.xml');
+[patt,~,~,~]=PatternsFromFairSimXml('./Data/FairSimParameters.xml');
+
+% Display
+figure;
+for ii=1:9
+    subplot(3,3,ii); imdisp(patt(:,:,ii),['Pattern #',num2str(ii)],0);
+end
